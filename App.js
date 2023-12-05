3bc4b9b0c9f9d39bfe74419d00d1ac616ab858dc
Maria Elena Massarelli <111201881+mariaelenamas@users.noreply.github.com>
2023-12-06 00:58:28 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,53 @@
+import Start from "./components/Start.js";
+import Chat from "./components/Chat.js";
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  const [text, setText] = useState('');
+
+  // alert the user input (`text` state's value)
+  const alertMyText = () => {
+    Alert.alert(text);
+  }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Start"
+      >
+        <Stack.Screen
+          name="Start"
+          component={Start}
+        />
+        <Stack.Screen
+          name="Chat"
+          component={Chat}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  textInput: {
+    width: '88%',
+    borderWidth: 1,
+    height: 50,
+    padding: 10
+  },
+  textDisplay: {
+    height: 50,
+    lineHeight: 50
+  }
+});
+
+export default App;
