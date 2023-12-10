8b4649b67d79700db43c1d47a550bf7edb0f2fce
Maria Elena <111201881+MariaElenaMas@users.noreply.github.com>
2023-12-10 20:50:02 +0100

    Bubble and GiftedChat added

--- components/Chat.js
+++ components/Chat.js
@@ -1,19 +1,56 @@
-import { useEffect } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { useState, useEffect } from "react";
+import { StyleSheet, View, KeyboardAvoidingView } from "react-native";
+import { Bubble, GiftedChat } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
     const { name } = route.params;
+    const [messages, setMessages] = useState([]);
+    const onSend = (newMessages) => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+    }
+
+    useEffect(() => {
+        setMessages([
+            {
+                _id: 1,
+                text: "Hello developer",
+                createdAt: new Date(),
+                user: {
+                    _id: 2,
+                    name: "React Native",
+                    avatar: "https://placeimg.com/140/140/any",
+                },
+            },
+            {
+                _id: 2,
+                text: 'This is a system message',
+                createdAt: new Date(),
+                system: true,
+            },
+        ]);
+    }, []);
 
     useEffect(() => {
         navigation.setOptions({ title: name });
     }, []);
 
     return (
-        <View style={styles.container}>
-            <Text>Hello Chat!</Text>
-        </View>
+        <KeyboardAvoidingView
+            style={{ flex: 1 }}
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
+            <GiftedChat
+                messages={messages}
+                renderBubble={renderBubble}
+                onSend={(messages) => onSend(messages)}
+                user={{
+                    _id: 1,
+                }}
+            />
+        </KeyboardAvoidingView>
     );
-}
+};
+
 
 const styles = StyleSheet.create({
     container: {
@@ -22,5 +59,19 @@ const styles = StyleSheet.create({
         alignItems: "center"
     }
 });
+
+const renderBubble = (props) => {
+    return <Bubble
+        {...props}
+        wrapperStyle={{
+            right: {
+                backgroundColor: "#000"
+            },
+            left: {
+                backgroundColor: "#FFF"
+            }
+        }}
+    />
+}
 
 export default Chat;
