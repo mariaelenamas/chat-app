e8aca5eeb2ce743155a733e9125a8668682558f0
mariaelenamas <mariaelenamassarelli@gmail.com>
2023-12-18 00:21:57 +0100

    File updated for firebaseConfig

--- App.js
+++ App.js
@@ -1,30 +1,55 @@
 // import react Navigation
-import Start from "./components/Start.js";
-import Chat from "./components/Chat.js";
+import Start from "./components/Start";
+import Chat from "./components/Chat";
 
 // import react Navigation
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(["AsyncStorage has been extracted from"]);
 
 // Create the navigator
 const Stack = createNativeStackNavigator();
 
 const App = () => {
 
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDu36bhkvWUPr1QbKCTAlVO8o2eSEEOdbI",
+    authDomain: "meetapp-f1734.firebaseapp.com",
+    projectId: "meetapp-f1734",
+    storageBucket: "meetapp-f1734.appspot.com",
+    messagingSenderId: "1083826638375",
+    appId: "1:1083826638375:web:64e9c178756a6f29a24802"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
+  // Initialize the storage handler
+  const storage = getStorage(app);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Start"
-      >
-        <Stack.Screen
-          name="Start"
-          component={Start}
-        />
-        <Stack.Screen
-          name="Chat"
-          component={Chat}
-        />
+      <Stack.Navigator initialRouteName="Start">
+        <Stack.Screen name="Start" component={Start}></Stack.Screen>
+        <Stack.Screen name="Chat">
+          {(props) => (
+            <Chat
+              db={db}
+              {...props}
+            />
+          )}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
